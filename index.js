d7fdcec092d5aa67fcb8e358dc385631a5b63c4e
Donny Hardyanto <supervideoconference@gmail.com>
2020-01-19 22:04:22 +0700

    - fix encypt/decrypt

--- index.js
+++ index.js
@@ -1,72 +1,74 @@
 'use strict'
 const shortid = require('shortid')
 const Blowfish = require('egoroof-blowfish')
-const { isBrowser } = require('browser-or-node')
+const {isBrowser} = require('browser-or-node')
 let crypto = null
 if (isBrowser) {
-  crypto = require('crypto-browserify')
+    crypto = require('crypto-browserify')
 } else {
-  crypto = require('crypto')
+    crypto = require('crypto')
 }
 
 const Duckeys = {
-  generateSalt: function () {
-    return shortid.generate() + shortid.generate()
-  },
-  hash: function (d) {
-    const sha256 = crypto.createHash('sha256')
-    sha256.update(d)
-    return sha256.digest('base64')
-  },
-  deriveFrom: function (a, b) {
-    let self = this
-    return {
-      a: self.hash(a + b),
-      b: self.hash(b + a)
+    generateSalt: function () {
+        return shortid.generate() + shortid.generate()
+    },
+    hash: function (d) {
+        const sha256 = crypto.createHash('sha256')
+        sha256.update(d)
+        return sha256.digest('base64')
+    },
+    deriveFrom: function (a, b) {
+        let self = this
+        return {
+            a: self.hash(a + b),
+            b: self.hash(b + a)
+        }
+    },
+    encrypt: function (key, data) {
+        let dataHash = this.hash(data)
+        let dataLength = data.length
+        let x = dataLength % 8
+        if (x > 0) data = data + "\x01" + "-".repeat(8 - x)
+        let salt = shortid.generate()
+        let saltLength = salt.length
+        let rawData = saltLength.toString() + "\x01" + salt + "\x01" + dataHash + "\x01" + dataLength.toString() + "\x01" + data
+        let iv = new Uint8Array(8)
+        const bf = new Blowfish(key, Blowfish.MODE.CBC, Blowfish.PADDING.SPACE)
+        bf.setIv(iv)
+        const encryptedData = bf.encode(rawData)
+        let combinedEncryptedData = new Uint8Array(8 + encryptedData.length)
+        combinedEncryptedData.set(iv, 0)
+        combinedEncryptedData.set(encryptedData, 8)
+        return combinedEncryptedData
+    },
+    shim_atob: function (s) {
+        try {
+            return atob(s)
+        } catch (err) {
+            return require('atob').atob(s)
+        }
+    },
+    decrypt: function (key, encryptedData) {
+        let iv = encryptedData.subarray(0, 8)
+        let baseEncyptedData = encryptedData.subarray(8)
+        const bf = new Blowfish(key, Blowfish.MODE.CBC, Blowfish.PADDING.SPACE)
+        bf.setIv(iv)
+        let decryptedData = bf.decode(baseEncyptedData, Blowfish.TYPE.STRING)
+        let decryptedDataArray = decryptedData.split("\x01")
+        let datahash = decryptedDataArray[2]
+        let dataLength = parseInt(decryptedDataArray[3])
+        let data = decryptedDataArray[4]
+        data.slice(0, dataLength - 1)
+        let calculatedDataHash = this.hash(data)
+        if (calculatedDataHash!=datahash) {
+          throw new Error('KeyManager: Hash not consistent')
+        }
+        return data
     }
-  },
-  encrypt: function (key, data) {
-    let dataHash = this.hash(data)
-    let dataLength = data.length
-    let x = dataLength % 8
-    if (x > 0) data = data + ' '.repeat(x)
-    let salt = shortid.generate()
-    let saltLength = salt.length
-    let rawData = saltLength.toString() + ' ' + salt + ' ' + dataHash + ' ' + dataLength.toString() + ' ' + data
-    return rawData
-    /* const bf = new Blowfish(key, Blowfish.MODE.CBC, Blowfish.PADDING.NULL)
-    bf.setIv('12345678')
-    const encryptedData = bf.encode(rawData)
-    return encryptedData */
-    // return btoa(String.fromCharCode.apply(null, encryptedData))
-  },
-  shim_atob: function (s) {
-    try {
-      return atob(s)
-    } catch (err) {
-      return require('atob').atob(s)
-    }
-  },
-  decrypt: function (key, encryptedData) {
-    /*
-    let s1 = this.shim_atob(encryptedData)
-    let uint8ArrayDecoded = new Uint8Array(s1.split('').map(function (c) {
-      return c.charCodeAt(0)
-    }))
-    const bf = new Blowfish(key, Blowfish.MODE.CBC, Blowfish.PADDING.NULL)
-    bf.setIv('12345678')
-    const decryptedData = bf.decode(encryptedData, Blowfish.TYPE.STRING)
-    */
-    let decryptedData = encryptedData
-    let decryptedDataArray = decryptedData.split(' ', 5)
-    let dataLength = parseInt(decryptedDataArray[3])
-    let data = decryptedDataArray[4]
-    data.slice(0, dataLength - 1)
-    return data
-  }
 }
 
 const duckeys = {
-  Duckeys
+    Duckeys
 }
 module.exports = duckeys
